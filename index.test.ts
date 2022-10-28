fc0119b50c41951fc259bdccb9810c9331d675fc
Daniel Cassidy <mail@danielcassidy.me.uk>
2022-10-28 01:44:52 +0100

    feat(iterator): add iterator function

--- index.test.ts
+++ index.test.ts
@@ -1,3 +1,11 @@
 import test from "ava";
+import {iterator} from "./index";
 
-test("placeholder test", t => t.true(true));
+test("iterator", t => {
+    t.true(iterator([]).next().done);
+    const a = [1, 2, 3];
+    const it = a[Symbol.iterator]();
+    t.false(iterator(a).next().done);
+    t.is(iterator(a).next().value, 1);
+    t.is(iterator(it), it);
+});
--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
+import {hasProperty} from "unknown";
+
 export type IteratorLike<T> = Iterator<T> | Iterable<T>;
+
+export function iterator<T>(iterator: IteratorLike<T>): Iterator<T> {
+    return hasProperty(iterator, Symbol.iterator) ? iterator[Symbol.iterator]() : iterator;
+}
