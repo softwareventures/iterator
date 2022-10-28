135c5df6a6d96d77d6f6be8cb8506700734bdd4e
Daniel Cassidy <mail@danielcassidy.me.uk>
2022-10-28 03:12:34 +0100

    feat(toArrayOnce): add toArrayOnce function

--- index.ts
+++ index.ts
@@ -5,3 +5,16 @@ export type IteratorLike<T> = Iterator<T> | Iterable<T>;
 export function iterator<T>(iterator: IteratorLike<T>): Iterator<T> {
     return hasProperty(iterator, Symbol.iterator) ? iterator[Symbol.iterator]() : iterator;
 }
+
+const toIterator = iterator;
+
+export function toArrayOnce<T>(iterator: IteratorLike<T>): T[] {
+    const it = toIterator(iterator);
+    const array: T[] = [];
+    let element = it.next();
+    while (element.done !== true) {
+        array.push(element.value);
+        element = it.next();
+    }
+    return array;
+}
