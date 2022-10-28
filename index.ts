e0afdfd383e95d5451cb606bb488762514e70da4
Daniel Cassidy <mail@danielcassidy.me.uk>
2022-10-28 03:16:23 +0100

    feat(toSetOnce): add toSetOnce function

--- index.ts
+++ index.ts
@@ -18,3 +18,14 @@ export function toArrayOnce<T>(iterator: IteratorLike<T>): T[] {
     }
     return array;
 }
+
+export function toSetOnce<T>(iterator: IteratorLike<T>): Set<T> {
+    const it = toIterator(iterator);
+    const set = new Set<T>();
+    let element = it.next();
+    while (element.done !== true) {
+        set.add(element.value);
+        element = it.next();
+    }
+    return set;
+}
